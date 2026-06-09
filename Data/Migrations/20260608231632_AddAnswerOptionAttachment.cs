e88d337df35c08c91da76569fdc3ab64e9b8c9cd
Pampers432 <max23012007@gmail.com>
2026-06-09 15:12:58 +0300

    добавил кастомные ошибки и возможность добавлять картинки в вопросы

---
+++ Data/Migrations/20260608231632_AddAnswerOptionAttachment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnswerOptionAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Attachment",
+                table: "AnswerOptions",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attachment",
+                table: "AnswerOptions");
+        }
+    }
+}
